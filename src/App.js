44455994fe566e80e1667bd7bb0dbc084f6ba24d
ctrl07 <skgaming7869@gmail.com>
2023-12-23 20:17:16 +0530

    Simplify rendering in App.js used map for expenses

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      location: "Pune",
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      location: "Mumbai",
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      location: "Bangalore",
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      location: "Delhi",
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
     <div>
       <h1>Expense Tracker</h1>
-      <ExpenseItem></ExpenseItem>
+      {expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          location={expense.location}
+          date={expense.date}
+        />
+      ))}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,16 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
-function ExpenseItem (){
-    const expenseDate = new Date(2023, 13, 23);
-    const expenseItem = 'Food';
-    const expenseAmount = 10;
-    const locationOfExpenditure = 'Pune'
-    return ( 
-        <div className='expense-item'>
-            <div>{expenseDate.toISOString()}</div>
-            <div className='expense-item__description'>
-                <h2>{expenseItem}</h2>
-                <div className='expense-item__description'>{locationOfExpenditure}</div>
-                <div className='expense-item__price'>₹ {expenseAmount}</div>
-            </div>    
-    </div> )
-};
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__description">{props.location}</div>
+        <div className="expense-item__price">₹ {props.amount}</div>
+      </div>
+    </div>
+  );
+}
 
 export default ExpenseItem;
