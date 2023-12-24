be85f0f5feb750f290b7c61e8dfbf9dd4c897a54
ctrl07 <skgaming7869@gmail.com>
2023-12-25 04:53:06 +0530

    Button: Added Delete button. -Learn eventHandlers in React.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,11 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const deleteHandler = (e) => {
+    const parent = e.target.parentElement.parentElement;
+    parent.removeChild(e.target.parentElement);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -11,6 +16,8 @@ const ExpenseItem = (props) => {
         <div>{props.location}</div>
         <div className="expense-item__price">₹{props.amount}</div>
       </div>
+      <button>Change Title</button>
+      <button onClick={deleteHandler}>Delete</button>
     </Card>
   );
 };
