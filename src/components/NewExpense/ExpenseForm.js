b0542e18472e022ee250b505b70f99e1906ce693
ctrl07 <skgaming7869@gmail.com>
2023-12-29 01:48:11 +0530

    Assignment: Form visibility toggle.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -5,17 +5,18 @@ const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
+  const [isFormVisible, setIsFormVisible] = useState(false);
 
-  const titleChangeHandler = (e) => {
-    setEnteredTitle(e.target.value);
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
   };
 
-  const amountChangeHandler = (e) => {
-    setEnteredAmount(e.target.value);
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
   };
 
-  const dateChangeHandler = (e) => {
-    setEnteredDate(e.target.value);
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
   };
 
   const submitHandler = (event) => {
@@ -32,44 +33,59 @@ const ExpenseForm = (props) => {
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
+    setIsFormVisible(false);
+  };
+
+  const toggleFormVisibility = () => {
+    setIsFormVisible((prevVisible) => !prevVisible);
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__controls">
-          <label>Title</label>
-          <input
-            type="text"
-            value={enteredTitle}
-            onChange={titleChangeHandler}
-          />
-        </div>
-        <div className="new-expense__controls">
-          <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            value={enteredAmount}
-            onChange={amountChangeHandler}
-          />
-        </div>
-        <div className="new-expense__controls">
-          <label>Date</label>
-          <input
-            type="date"
-            min="2019-01-01"
-            max="2022-12-31"
-            value={enteredDate}
-            onChange={dateChangeHandler}
-          />
-        </div>
-        <div className="new-expense__actions">
-          <button type="submit">Add Expense</button>
-        </div>
-      </div>
-    </form>
+    <div>
+      {isFormVisible && (
+        <form action="" onSubmit={submitHandler}>
+          <div className="new-expense__controls">
+            <div className="new-expense__controls">
+              <label>Title</label>
+              <input
+                type="text"
+                value={enteredTitle}
+                onChange={titleChangeHandler}
+              />
+            </div>
+            <div className="new-expense__controls">
+              <label>Amount</label>
+              <input
+                type="number"
+                min="0.01"
+                step="0.01"
+                value={enteredAmount}
+                onChange={amountChangeHandler}
+              />
+            </div>
+            <div className="new-expense__controls">
+              <label>Date</label>
+              <input
+                type="date"
+                min="2019-01-01"
+                max="2022-12-31"
+                value={enteredDate}
+                onChange={dateChangeHandler}
+              />
+            </div>
+            <div className="new-expense__actions">
+              <button type="button" onClick={toggleFormVisibility}>
+                Cancel
+              </button>
+              <button type="submit">Add Expense</button>
+            </div>
+          </div>
+        </form>
+      )}
+      <button onClick={toggleFormVisibility}>
+        {isFormVisible ? "Cancel" : "Add Expense"}
+      </button>
+    </div>
   );
 };
 
